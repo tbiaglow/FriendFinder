51fd02148c005d952955a9afeac242ec2b40c210
tbiaglow <tbiaglow@gmail.com>
2019-07-25 19:36:30 -0400

    Set up friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+var friends = [
+    {"name":"Ahmed", "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]},
+    {"name":"Bob", "scores":[4, 2, 3, 5, 4, 2, 1, 4, 5, 2]   },
+    {"name":"Chad", "scores":[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]},
+    {"name":"Brad", "scores":[4, 4, 4, 4, 4, 4, 4, 4, 4, 4]},
+    {"name":"Stacy", "scores":[2, 4, 5, 1, 2, 1, 1, 2, 3, 4]},
+    {"name":"Gertrude", "scores":[5, 1, 2, 1, 5, 4, 1, 5, 2, 5]},
+    {"name":"Rebecca", "scores":[1, 5, 4, 4, 4, 2, 5, 1, 4, 2]},
+    {"name":"Tyrone", "scores":[2, 3, 1, 5, 1, 2, 3, 2, 3, 1]},
+    {"name":"Bill", "scores":[1, 1, 1, 2, 2, 2, 3, 3, 3, 4]},
+    {"name":"Chuck", "scores":[5, 5, 5, 4, 4, 4, 3, 3, 3, 2]}
+];
+  
