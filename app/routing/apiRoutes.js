e98e9d64e209072afec818d280467a684b9d365d
tbiaglow <tbiaglow@gmail.com>
2019-08-04 02:13:14 -0400

    moved difference logic to its proper place

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,32 @@
+var path = require('path');
+var friends = require("../data/friends.js");
+
+module.exports = function(app) {
 app.get("/api/friends", function(req, res) {
     return res.json(friends);
 });
 
 app.post("/api/friends", function(req, res) {
-    var newFriend = req.body;
-    console.log(newFriend);
-    friends.push(newFriend);
-    res.json(newFriend);
+var newFriend = req.body;
+console.log(newFriend);
+
+var difference = []
+for (i = 0; i < friends.length; i++) {
+    for (i = 0; i < data.scores.length; i++) {
+        difference[i] += Math.abs(friends.scores[i] - newFriend.scores[i])
+    }
+    var min = difference[0];
+    var minIndex = 0;
+
+    for (var i = 1; i < difference.length; i++) {
+        if (difference[i] < max) {
+            minIndex = i;
+            min = difference[i];
+        }
+    }
+}
+
+friends.push(newFriend);
+res.json(newFriend);
 });
+}
--- server.js
+++ server.js
@@ -12,8 +12,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require(path.join(_dirname, './routes/apiRoutes'))(app);
+require(path.join(_dirname, './routes/htmlRoutes'))(app);
 
 
 // Starts the server to begin listening
